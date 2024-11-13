f665dfbf9ea5d18146263da5daefdf6fb00f9b77
harutiro <harutiro2027@outlook.jp>
2024-11-14 08:03:44 +0900

    add: ログインをしていないと表示をできないように変更

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,15 +1,28 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="#">スケジュール管理アプリ</a>
+    <a class="navbar-brand" href="{{route("ScheduleList")}}">スケジュール管理アプリ</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
         <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="#">全てのスケジュール <span class="sr-only"></span></a>
+            <a class="nav-item nav-link active" href="{{route("ScheduleList")}}">全てのスケジュール <span class="sr-only"></span></a>
             <a class="nav-item nav-link" href="#">週間スケジュール</a>
             <a class="nav-item nav-link" href="#">月間スケジュール</a>
             <a class="nav-item nav-link" href="{{route("ScheduleCreate")}}">新規スケジュール登録</a>
         </div>
+    </div>
+    <!-- ログイン状態表示 -->
+    <div class="navbar-nav">
+        @if(Auth::check())
+        <span class="navbar-text">ログイン中：{{Auth::user()->name}}</span>｀
+        <form method="POST" action="{{route("logout")}}">
+            @csrf
+            <button type="submit" class="btn btn-danger">ログアウト</button>
+        </form>
+        @else
+        <a class="nav-item nav-link" href="{{route("login")}}">ログイン</a>
+        <!-- <a class="nav-item nav-link" href="{{route("login")}}">ユーザー登録</a> -->
+        @endif
     </div>
 </nav>
--- routes/web.php
+++ routes/web.php
@@ -8,24 +8,13 @@ Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('login', [LoginController::class, 'login']);
 Route::post('logout', [LoginController::class, 'logout'])->name('logout');
 
-// スケジュール覧画面
-Route::get('/', [ScheduleController::class, 'showList'])->name('ScheduleList');
-
-
-// スケジュール作成画面
-Route::get('/schedule/create', [ScheduleController::class, 'showCreate'])->name('ScheduleCreate');
-// スケジュール作成処理
-Route::post('/schedule/store', [ScheduleController::class, 'exeStore'])->name('ScheduleStore');
-
-
-// スケジュール編集画面
-Route::get('/schedule/edit/{id}', [ScheduleController::class, 'showEdit'])->name('ScheduleEdit');
-// スケジュール編集処理
-Route::post('/schedule/update', [ScheduleController::class, 'exeUpdate'])->name('ScheduleUpdate');
-
-// スケジュール削除処理
-Route::post('/schedule/delete/{id}', [ScheduleController::class, 'exeDelete'])->name('ScheduleDelete');
-
-
-// スケジュール詳細画面
-Route::get('/schedule/{id}', [ScheduleController::class, 'showDetail'])->name('ScheduleDetail');
+// 認証が必要なルート
+Route::middleware(['auth'])->group(function () {
+    Route::get('/', [ScheduleController::class, 'showList'])->name('ScheduleList');
+    Route::get('/schedule/create', [ScheduleController::class, 'showCreate'])->name('ScheduleCreate');
+    Route::post('/schedule/store', [ScheduleController::class, 'exeStore'])->name('ScheduleStore');
+    Route::get('/schedule/edit/{id}', [ScheduleController::class, 'showEdit'])->name('ScheduleEdit');
+    Route::post('/schedule/update', [ScheduleController::class, 'exeUpdate'])->name('ScheduleUpdate');
+    Route::get('/schedule/{id}', [ScheduleController::class, 'showDetail'])->name('ScheduleDetail');
+    Route::post('/schedule/delete/{id}', [ScheduleController::class, 'exeDelete'])->name('ScheduleDelete');
+});
